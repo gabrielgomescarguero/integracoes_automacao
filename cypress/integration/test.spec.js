8f09e633818c64fdaf20fc09a342731c895ccf27
Gabriel Gomes <gabriel.silva@MacBook-Pro-de-Gabriel.local>
2021-10-19 15:42:45 -0300

    first cypress project integration

---
+++ cypress/integration/test.spec.js
@@ -0,0 +1,45 @@
+describe('Mocha’s interface', () => {
+    context('it provides a way to keep tests easier to read and organized.', () => {
+      it('This is your test case', () => {
+        expect(true).to.eq(true)
+      });
+    });
+  });
+
+
+describe('Products api', () => {
+  context('GET /produtos', () => {
+      it('should return a list with all products', () => {
+          cy.request({
+              method: 'GET',
+              url: 'https://serverest.dev/produtos'
+          })
+              .should((response) => {
+                  cy.log(JSON.stringify(response.body))
+              });
+      });
+  });
+});
+
+
+
+
+
+describe('Products api', () => {
+  context('GET /produtos', () => {
+      it('should return a list with all products', () => {
+          cy.request({
+              method: 'GET',
+              url: 'https://serverest.dev/produtos'
+          })
+              .should((response) => {
+                  expect(response.status).to.eq(200)
+                  expect(response.body.quantidade).to.eq(50)
+                  expect(response.body.produtos.length).to.be.eq(50);
+                  expect(response.body.produtos[0]).to.have.all.keys(
+                    'nome', 'preco', 'descricao', 'quantidade', '_id'
+                  )
+              });
+      });
+  });
+});
